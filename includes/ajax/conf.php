e741b4c37d9386719b3ed6cd19daee1ff0dc1b92
Kaylleur <dev@thomas-brunet.fr>
2017-01-29 19:28:05 +0100

    add conf on casperjs script

---
+++ includes/ajax/conf.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: Thomas
+ * Date: 29/01/2017
+ * Time: 19:26
+ */
+
+$CONFIGURATION = [];
+
+$CONFIGURATION['casperjsScript'] = "casperjs C:/wamp64/www/Crowler/casper/index.js";
